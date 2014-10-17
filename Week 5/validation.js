c16f3592fa38551f492c5127335dd8393fefa633
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-10-17 15:26:28 +0100

    final lab 5 version

--- Week 5/validation.js
+++ Week 5/validation.js
@@ -12,12 +12,39 @@ function validateForm1 () {
     var gender = document.forms[formName]["gender"].value;
     var conditionsAccepted = document.getElementById('tandcCheckbox').checked;
 
-    if(isValidValue(name) && isValidValue(age) && isValidValue(gender) && conditionsAccepted){
+    var et = "";
+    var haserror = false;
+
+    if (!isValidValue(name))
+    {
+        haserror = true;
+        et+="Name not valid<br>";
+    }
+    if(!isValidValue(age))
+    {
+        haserror = true;
+        et+= "Age not valid<br>";
+    }
+    if(!isValidValue(gender))
+    {
+        haserror = true;
+        et += "No gender selected<br>";
+    }
+    if(! conditionsAccepted)
+    {
+        haserror = true;
+        et+="You must accept the terms and conditions";
+    }
+
+    if(!haserror){
         error.innerHTML = '';
         alert("Woohoo!\nName: " +name + "\nAge: " + age + "\nGender: " + gender + "\nT&Cs Accepted: " + conditionsAccepted);
+        return true;
     }
     else{
-        error.innerHTML = 'Error with your entry';
+        error.innerHTML =et;
+        return false;
+
 
     }
 
