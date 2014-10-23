2b3a474ed9af997f2120440a8d49f0dec59f11b1
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-10-23 16:50:36 +0100

    finished up week 6

--- Week 6/wiki.js
+++ Week 6/wiki.js
@@ -4,9 +4,26 @@ var hideSections = function(){
     var elements = document.getElementsByTagName("section");
 
     for (var i = elements.length - 1; i >= 0; i--) {
-        elements[i].style.display = hideShowButton.innerHTML == "HIDE" ? "none" : "inline";
+        elements[i].style.display = hideShowButton.innerHTML == "HIDE" ? "none" : "block";
     };
 
     hideShowButton.innerHTML = hideShowButton.innerHTML == "HIDE" ? "SHOW" : "HIDE";
 
+}
+
+
+function emphasise(){
+
+
+    var r = new RegExp('defeasible', 'ig');
+
+    document.getElementById('body').innerHTML = document.getElementById('body').innerHTML.replace(r, "<b style='color: yellow'>Defeasible</b>");
+
+}
+
+function removeLinks(){
+    var anchors = document.getElementsByTagName("a");
+    for (var i = anchors.length - 1; i >= 0; i--) {
+        anchors[i].parentNode.removeChild(anchors[i]);
+    };
 }
