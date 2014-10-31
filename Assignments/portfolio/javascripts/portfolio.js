874a6d38f0b254a2ed3d70139f8b87a63fdec80d
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-10-31 21:43:32 +0000

    more content and comments

--- Assignments/portfolio/javascripts/portfolio.js
+++ Assignments/portfolio/javascripts/portfolio.js
@@ -4,20 +4,22 @@ function onLoadHandler(){
 }
 
 
+//The handler for the 'show nav' button on mobile:
 function showOrHideNav(){
     var nav = document.getElementById("topOfPageNav");
     var button = document.getElementById("showOrHideNavButton");
     console.log(button.innerHTML);
     nav.style.display = button.innerHTML == "Show Navigation" ? "inline-block" : "none";
     button.innerHTML = button.innerHTML == "Show Navigation" ? "Hide Navigation" : "Show Navigation";
-
 }
 
+// This function calculates my age based on my birthday
+// Then it puts it into the 'about' section
 var setAge = function(){
     var ageSpan = document.getElementById("age");
     var birthday = new Date(1991,11,14);
     var timeDifference = (new Date().getTime() - birthday.getTime());
-    var day = 1000 * 60 * 60 * 24;
+    var day = 1000 * 60 * 60 * 24; //(time difference is given in seconds, let's convert to days)
     var days = Math.floor(timeDifference/day);
     var months = Math.floor(days/31);
     var years = Math.floor(months/12);
