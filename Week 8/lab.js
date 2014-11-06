e018690f5ed8f6460834ae7b27067dbb5b41e9a0
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-11-06 03:16:33 +0000

    everything works now

--- Week 8/lab.js
+++ Week 8/lab.js
@@ -2,26 +2,31 @@
 $('a').css("font-style", "italic");
 
 //Task 2
-$('#external_links_section').after().load('newSection.txt');
+var newSection = $('<section id="newSection"></section">');
+newSection.load('newSection.txt');
+newSection.insertAfter('#external_links_section');
 
 //Task 3 & 4
-$('section').each(function(){
-    var name = $(this).find("h1").eq(0).html();
-    var buttonText = "Close " + name;
-    var button = $('<button>'+buttonText+'</button><br>');
-    var sect = $(this);
-    button.click(function(){
-        if(sect.is(":visible"))
-        {
-            sect.hide();
-            $(this).text("Open " + name); //Task 4
-        }
-        else
-        {
-            sect.show();
-             $(this).text("Close " + name); //Task 4
-        }
-    });
+$('document').ready(function(){
+    $('section').each(function(){
+        var name = $(this).find("h1").eq(0).html();
+        var buttonText = "Close " + name;
+        var button = $('<button>'+buttonText+'</button><br>');
+        var sect = $(this);
 
-    $(this).before(button);
+        button.click(function(){
+            if(sect.is(":visible"))
+            {
+                sect.hide();
+                $(this).text("Open " + name); //Task 4
+            }
+            else
+            {
+                sect.show();
+                 $(this).text("Close " + name); //Task 4
+            }
+        });
+
+        $(this).before(button);
+    });
 });
