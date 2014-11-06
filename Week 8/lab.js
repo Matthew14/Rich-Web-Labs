75955990f7e32811f959a2414f149979488ae17f
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-11-06 02:40:43 +0000

    fixed  task 2

--- Week 8/lab.js
+++ Week 8/lab.js
@@ -2,9 +2,9 @@
 $('a').css("font-style", "italic");
 
 //Task 2
-$('#newStuff').load('newSection.txt');
+$('#external_links_section').after().load('newSection.txt');
 
-//Task 3
+//Task 3 & 4
 $('section').each(function(){
     var name = $(this).find("h1").eq(0).html();
     var buttonText = "Close " + name;
@@ -14,12 +14,12 @@ $('section').each(function(){
         if(sect.is(":visible"))
         {
             sect.hide();
-            $(this).text("Open " + name);
+            $(this).text("Open " + name); //Task 4
         }
         else
         {
             sect.show();
-             $(this).text("Close " + name);
+             $(this).text("Close " + name); //Task 4
         }
     });
 
