67fd71ec97c5f58bb556aa88a3a2d8a6fa7e60ba
Matthew O'Neill <14matthewoneill14@gmail.com>
2014-11-06 02:36:16 +0000

    all done

--- Week 8/lab.js
+++ Week 8/lab.js
@@ -1,11 +1,27 @@
-var linksAreItalic = false;
+//Task 1
+$('a').css("font-style", "italic");
 
-$('#makeLinksItalicButton').click(function(){
-    $('a').css("font-style", linksAreItalic ? "normal": "italic");
-    linksAreItalic = !linksAreItalic;
-    $(this).text(linksAreItalic ?  "Make Links not Italic": "Make Links Italic" );
-});
+//Task 2
+$('#newStuff').load('newSection.txt');
 
-$('#addExternalContent').click(function(){
-    $('#newStuff').load('newSection.txt');
+//Task 3
+$('section').each(function(){
+    var name = $(this).find("h1").eq(0).html();
+    var buttonText = "Close " + name;
+    var button = $('<button>'+buttonText+'</button><br>');
+    var sect = $(this);
+    button.click(function(){
+        if(sect.is(":visible"))
+        {
+            sect.hide();
+            $(this).text("Open " + name);
+        }
+        else
+        {
+            sect.show();
+             $(this).text("Close " + name);
+        }
+    });
+
+    $(this).before(button);
 });
